3d057af091b30cb1ba8cb2a3ac9eb6d65a6bb28a
drohatgi <dwr500@gmail.com>
2018-11-03 15:49:36 -0400

    Initial files

---
+++ main.py
@@ -0,0 +1,90 @@
+import inputReader
+import audioPreprocessor as ap
+import net
+
+
+import torch
+import torch.nn.functional as F
+
+import random
+
+SNIPPET_WINDOW = 0.5
+SNIPPET_HOP = SNIPPET_WINDOW/2
+FREQUENCY = 22050
+FFT_WINDOW = 1024
+FFT_HOP = 512
+EPOCHS = 10
+LR = 0.01
+
+TRAIN_BATCH = 64
+
+THRESHOLD = 0.1
+
+instruments = ["cla", "flu", "gac", "gel", "org", "pia", "sax", "tru", "vio", "voi"]
+
+def encodeLabels(labels):
+	if not type(labels) == list:
+		labels = [labels]
+	vec = [0]*len(instruments)
+	for label in labels:
+		vec[instruments.index(label)] = 1
+	return vec
+
+def processSnippet(snippet):
+	snippet = ap.toMono(snippet)
+	snippet = ap.downsample(snippet, FREQUENCY)
+	snippet = ap.normalize(snippet)
+	snippet = ap.stft(snippet, FFT_WINDOW, FFT_HOP)
+	snippet = ap.melSpectrogram(snippet)
+	snippet = ap.logCompress(snippet)
+	return snippet
+
+def batchify(procData, batchsize):
+	random.shuffle(procData)
+	L = zip(*procData)
+	M = []
+	for iStart in range(0, len(procData), batchsize):
+		obj = []
+		for i in range(len(L)):
+			obj.append(torch.Tensor(L[i][iStart:iStart+batchsize]))
+		M.append(obj)
+	return M
+
+rawTrainingData = inputReader.readTrainingAudio()
+rawTestData = inputReader.readTestAudio()
+
+procTrainingData = []
+
+for (clip, label) in rawTrainingData:
+	snippets = ap.divide(clip, SNIPPET_WINDOW, SNIPPET_WINDOW)
+	labelvec = encodeLabels(label)
+	for snippet in snippets:
+		snippet = processSnippet(snippet)
+		procTrainingData.append((snippet, labelvec))
+
+procTrainingData = batchify(procTrainingData, TRAIN_BATCH)
+
+model = net.Net()
+optimizer = optim.Adam(model.parameters(), lr=LR)
+
+for epoch in range(1, EPOCHS+1):
+	net.train(model, procTrainingData, optimizer, epoch)
+
+procTestData = []
+
+loss = 0
+
+for i, (clip, labels) in enumerate(rawTestData):
+	snippets = ap.divide(clip, SNIPPET_WINDOW, SNIPPET_HOP)
+	labelvec = encodeLabels(label)
+	testData = []
+	for snippet in snippets:
+		snippet = processSnippet(snippet)
+		testData.append(snippet)
+	testData = torch.Tensor(testData)
+	output = net.test(model, testData)
+	output = torch.mean(output, 0)
+	output = (output >= THRESHOLD)	
+	loss += abs(output - labelvec)
+
+print(loss/len(rawTestData))
---
+++ net.py
@@ -0,0 +1,113 @@
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+import audioPreprocessor
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.channels = [1, 32, 64, 128]
+        self.finalConv1 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.finalConv2 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+
+    def forward_block(self, x, inChannels, outChannels):
+        self.conv1 = nn.Conv2d(inChannels, outChannels, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(outChannels, outChannels, kernel_size=3, padding=1)
+        self.pool3 = nn.MaxPool2d(kernel_size=3)
+        self.drop4 = nn.Dropout(p=0.25)
+
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.pool3(x)
+        x = self.drop4(x)
+        return x
+
+
+    def forward(self, x):
+        for i in range(len(channels) - 1):
+            x = self.forward_block(x, channels[i], channels[i+1])
+        x = F.relu(self.finalConv1(x))
+        x = F.relu(self.finalConv2(x))
+        x = nn.MaxPool2d(kernel_size=x.shape())(x)
+        x = x.view(-1, 256)
+        x = nn.Linear(256,1024)(x)
+        x = nn.Dropout(p=0.5)(x)
+        x = nn.Linear(1024, 11)(x)
+        x = nn.Sigmoid()(x)
+
+        return x
+
+def train(model, train_loader, optimizer, epoch):
+    model.train()
+    sum_num_correct = 0
+    sum_loss = 0
+    num_batches_since_log = 0
+    for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.cross_entropy(output, target)
+        pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+        correct = pred.eq(target.view_as(pred)).sum().item()
+        sum_num_correct += correct
+        sum_loss += loss.item()
+        num_batches_since_log += 1
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 50 == 0:
+            print('Train Epoch: {} [{:05d}/{} ({:02.0f}%)]\tLoss: {:.6f}\tAccuracy: {:02.0f}%'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item(), 
+                100. * sum_num_correct / (num_batches_since_log * train_loader.batch_size))
+            )
+            sum_num_correct = 0
+            sum_loss = 0
+            num_batches_since_log = 0
+
+def test(model, data):
+    model.eval()
+    with torch.no_grad():
+        output = model(data)
+    return output
+    
+
+def main():
+    ##################################################################
+    # NOTE - ONLY CHANGE THE HYPERPARAMETERS
+    ##################################################################
+
+    #-------------------------------------------------------------#
+    # SETUP - DO NOT CHANGE
+    #-------------------------------------------------------------#
+    args = dict()
+    args["seed"] = 1
+    args["no_cuda"] = False
+    args["log_interval"] = 50
+    args["batch_size"] = 64
+    args["test-batch-size"] = 1000
+
+    #-------------------------------------------------------------#
+    # HYPERPARAMETERS - CHANGE THIS BLOCK
+    #-------------------------------------------------------------#
+    params = dict()
+    params["epochs"] = 10
+    params["lr"] = 0.01
+    #-------------------------------------------------------------#
+    torch.manual_seed(args["seed"])
+
+    train_loader = tensorify(audioPreprocessor.getTrainingData())
+    test_loader = tensorify(audioPreprocessor.getTestData())
+
+    model = Net(params)
+    optimizer = optim.Adam(model.parameters(), lr=params["lr"])
+
+    for epoch in range(1, params["epochs"] + 1):
+        train(model, train_loader, optimizer, epoch)
+        test(model, test_loader)
+
+
+if __name__ == '__main__':
+    main()
