c49b88c4b3486e42b6b800acb13e2f831134cdc0
drohatgi <dwr500@gmail.com>
2018-12-07 21:31:27 -0500

    sadness incarnate

--- main.py
+++ main.py
@@ -97,8 +97,6 @@ except:
     pfh.close()
 
 #random.shuffle(procTrainingData)
-lst = range(len(procTrainingData))
-random.Random(4).shuffle(lst)
 random.Random(4).shuffle(procTrainingData)
 procTrainingData = procTrainingData[:2998+128*112] + procTrainingData[2998+128*113:]
 
